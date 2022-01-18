61bce991a604f5ffc35c2340d84cd421ea89777d
KeerthanaSiddula <97495783+KeerthanaSiddula@users.noreply.github.com>
2022-01-18 10:39:57 +0530

    Add files via upload

---
+++ SQL/Activities/Activity2.sql
@@ -0,0 +1,126 @@
+REM   Script: Activity2
+REM   Activity2
+
+CREATE TABLE salesman(salesman_id  int, salesman_name varchar2(20), salesman_city varchar2(20), commission int);
+
+DESCRIBE salesman
+
+
+CREATE TABLE salesman(salesman_id  int, salesman_name varchar2(20), salesman_city varchar2(20), commission int);
+
+DESCRIBE salesman
+
+
+Insert ALL 
+    INTO salesman VALUES (5001, "James Hoog", "New York", 15) 
+    INTO salesman VALUES (5002, "Nail Knite", "paris", 13) 
+    INTO salesman VALUES (5005, "Pit Alex", "London", 11) 
+    INTO salesman VALUES (5006, "McLyon", "Paris", 14) 
+    INTO salesman VALUES (5007, "Paul Adam", "Rome", 13) 
+    INTO salesman VALUES (5003, "Lauson Hen", "San Jose", 12) 
+SELECT 1 FROM DUAL;
+
+SELECT * from salesman;
+
+CREATE TABLE salesman(salesman_id  int, salesman_name varchar2(20), salesman_city varchar2(20), commission int);
+
+DESCRIBE salesman
+
+
+Insert ALL 
+    INTO salesman VALUES (5001, "James Hoog", "NewYork", 15) 
+    INTO salesman VALUES (5002, "Nail Knite", "paris", 13) 
+    INTO salesman VALUES (5005, "Pit Alex", "London", 11) 
+    INTO salesman VALUES (5006, "McLyon", "Paris", 14) 
+    INTO salesman VALUES (5007, "Paul Adam", "Rome", 13) 
+    INTO salesman VALUES (5003, "Lauson Hen", "SanJose", 12) 
+SELECT 1 FROM DUAL;
+
+SELECT * from salesman;
+
+CREATE TABLE salesman(salesman_id  int, salesman_name varchar2(20), salesman_city varchar2(20), commission int);
+
+DESCRIBE salesman
+
+
+Insert ALL 
+    INTO salesman VALUES (5001, "James Hoog", "NewYork", 15) 
+    INTO salesman VALUES (5002, "Nail Knite", "paris", 13) 
+    INTO salesman VALUES (5005, "Pit Alex", "London", 11) 
+    INTO salesman VALUES (5006, "McLyon", "Paris", 14) 
+    INTO salesman VALUES (5007, "Paul Adam", "Rome", 13) 
+    INTO salesman VALUES (5003, "Lauson Hen", "SanJose", 12) 
+SELECT 1 FROM DUAL;
+
+DESCRIBE salesman
+
+
+Insert ALL 
+    INTO salesman VALUES (5001, "James Hoog", "NewYork", 15) 
+    INTO salesman VALUES (5002, "Nail Knite", "paris", 13) 
+    INTO salesman VALUES (5005, "Pit Alex", "London", 11) 
+    INTO salesman VALUES (5006, "McLyon", "Paris", 14) 
+    INTO salesman VALUES (5007, "Paul Adam", "Rome", 13) 
+    INTO salesman VALUES (5003, "Lauson Hen", "SanJose", 12) 
+SELECT 1 FROM DUAL;
+
+SELECT * from salesman;
+
+SELECT * from salesman;
+
+CREATE TABLE salesman(salesman_id  int, salesman_name varchar2(20), salesman_city varchar2(20), commission int);
+
+DESCRIBE salesman
+
+
+Insert ALL 
+    INTO salesman VALUES (5001, "James Hoog", "NewYork", 15) 
+    INTO salesman VALUES (5002, "Nail Knite", "paris", 13) 
+    INTO salesman VALUES (5005, "Pit Alex", "London", 11) 
+    INTO salesman VALUES (5006, "McLyon", "Paris", 14) 
+    INTO salesman VALUES (5007, "Paul Adam", "Rome", 13) 
+    INTO salesman VALUES (5003, "Lauson Hen", "SanJose", 12) 
+SELECT 1 FROM DUAL;
+
+SELECT * from salesman;
+
+SELECT * from salesman;
+
+Insert ALL 
+    INTO salesman VALUES (5001, "James Hoog", "New York", 15) 
+    INTO salesman VALUES (5002, "Nail Knite", "paris", 13) 
+    INTO salesman VALUES (5005, "Pit Alex", "London", 11) 
+    INTO salesman VALUES (5006, "McLyon", "Paris", 14) 
+    INTO salesman VALUES (5007, "Paul Adam", "Rome", 13) 
+    INTO salesman VALUES (5003, "Lauson Hen", "San Jose", 12) 
+SELECT 1 FROM DUAL;
+
+Insert ALL 
+    INTO salesman VALUES (5001, "James Hoog", "New York", 15) 
+    INTO salesman VALUES (5002, "Nail Knite", "paris", 13) 
+    INTO salesman VALUES (5005, "Pit Alex", "London", 11) 
+    INTO salesman VALUES (5006, "McLyon", "Paris", 14) 
+    INTO salesman VALUES (5007, "Paul Adam", "Rome", 13) 
+    --INTO salesman VALUES (5003, "Lauson Hen", "San Jose", 12) 
+SELECT 1 FROM DUAL;
+
+Insert ALL 
+    INTO salesman(salesman_id, salesman_name, salesman_city, commission) VALUES (5001, "James Hoog", "New York", 15) 
+    INTO salesman(salesman_id, salesman_name, salesman_city, commission) VALUES (5002, "Nail Knite", "paris", 13) 
+    INTO salesman(salesman_id, salesman_name, salesman_city, commission) VALUES (5005, "Pit Alex", "London", 11) 
+    INTO salesman(salesman_id, salesman_name, salesman_city, commission) VALUES (5006, "McLyon", "Paris", 14) 
+    INTO salesman(salesman_id, salesman_name, salesman_city, commission) VALUES (5007, "Paul Adam", "Rome", 13) 
+    INTO salesman(salesman_id, salesman_name, salesman_city, commission) VALUES (5003, "Lauson Hen", "San Jose", 12) 
+SELECT 1 FROM DUAL;
+
+Insert ALL 
+    INTO salesman VALUES (5001, 'James Hoog', 'New York', 15) 
+    INTO salesman VALUES (5002, 'Nail Knite', 'paris', 13) 
+    INTO salesman VALUES (5005, 'Pit Alex', 'London', 11) 
+    INTO salesman VALUES (5006, 'McLyon', 'Paris', 14) 
+    INTO salesman VALUES (5007, 'Paul Adam', 'Rome', 13) 
+    INTO salesman VALUES (5003, 'Lauson Hen', 'San Jose', 12) 
+SELECT 1 FROM DUAL;
+
+SELECT * from salesman;
+
