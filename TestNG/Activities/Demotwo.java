03dcc263b74fccf874822cd37dafc9baf58e1215
KeerthanaSiddula <97495783+KeerthanaSiddula@users.noreply.github.com>
2022-01-31 22:44:21 +0530

    Add files via upload

---
+++ TestNG/Activities/Demotwo.java
@@ -0,0 +1,10 @@
+package Activities;
+
+import org.testng.annotations.Test;
+
+public class Demotwo {
+    @Test
+    public void Demotwotc(){
+        System.out.println("First test case in Demotwo");
+    }
+}
